facae0380e3dd4e0ac8470befae7187da0d6e6c5
willyam.barros <willyam.barros@techlead.com.br>
2025-08-04 14:58:59 -0300

    Projeto inicial gerado pelo Spring Initializr

---
+++ src/main/java/br/com/techlead/livros_api/LivrosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.techlead.livros_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/techlead/livros_api/LivrosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techlead.livros_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
