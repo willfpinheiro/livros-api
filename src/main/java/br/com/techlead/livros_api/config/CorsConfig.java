21b33f4588c11d21d76cb23e05183069e5665791
willyam.barros <willyam.barros@techlead.com.br>
2025-08-05 21:26:14 -0300

    liberar cors

--- src/main/java/br/com/techlead/livros_api/config/CorsConfig.java
+++ src/main/java/br/com/techlead/livros_api/config/CorsConfig.java
@@ -1,9 +1,11 @@
 package br.com.techlead.livros_api.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
